2a5c2b52d55a7be4f32a7b7a6a6c8a066f23b0d2
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2024-07-12 20:19:50 -0400

    deleted unnecessary files

--- mysql/replication_group_configuration_version.sql
+++
@@ -1,9 +0,0 @@
-create table mysql.replication_group_configuration_version
-(
-    name    char(255) charset ascii not null comment 'The configuration name.'
-        primary key,
-    version bigint unsigned         not null comment 'The version of the configuration name.'
-)
-    comment 'The group configuration version.' row_format = DYNAMIC
-                                               stats_persistent = 0;
-
